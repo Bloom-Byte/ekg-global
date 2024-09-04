69807214dee1e3ec13071a55d78d655fe8e03783
ti-oluwa <tioluwa.dev@gmail.com>
2024-09-04 10:39:28 +0100

    What: function selection for criteria creation completed

--- apps/risk_management/static/risk_management/scripts/functionsModal.js
+++ apps/risk_management/static/risk_management/scripts/functionsModal.js
@@ -1,9 +1,5 @@
-const functionsModal = document.querySelector("#functions-modal");
-const functionsModalCloseBtn = functionsModal.querySelector("");
-const optionsSearchInput = document.querySelector("#options-search-input");
-const MainOptionSet = functionsModal.querySelector("#function-options.options");
-const subOptionSets = functionsModal.querySelectorAll(".sub-options");
-const allOptions = functionsModal.querySelectorAll(".option");
+const functionsModals = document.querySelectorAll(".functions-modal");
+
 
 /**
  * Returns the visible element with the highest z-index from a given list of elements.
@@ -52,31 +48,109 @@ function getVisibleElementWithHighestZIndex(elements) {
 }
 
 
-// Function to handle search functionality
-optionsSearchInput.addEventListener('input', function () {
-    const filter = this.value.toLowerCase();
-    const OptionSets = [MainOptionSet, ...subOptionSets]
-    const targetOptionSet = getVisibleElementWithHighestZIndex(OptionSets);
+function getSubOptionFormFieldsData(formfieldsContainer) {
+    const data = {
+        "name": null,
+        "options": {}
+    };
+    const formFields = formfieldsContainer.querySelectorAll(".form-field");
 
-    targetOptionSet.querySelectorAll('.option').forEach(item => {
-        const text = item.textContent.toLowerCase();
-        item.style.display = text.includes(filter) ? '' : 'none';
+    formFields.forEach(formField => {
+        const formInput = formField.querySelector(".form-input");
+        data.options[formInput.name] = formInput.value;
     });
-});
+
+    data.name = formfieldsContainer.dataset.function;
+    return data;
+}
 
 
-allOptions.forEach(option => {
-    const optionLabel = option.querySelector(".option-label");
-    const subOptionSet = option.querySelector(".sub-options");
-    if (!subOptionSet) return;
+functionsModals.forEach(functionsModal => {
+    const functionsModalCloseBtn = functionsModal.querySelector(".modal-head .btn-close");
+    const optionsSearchInput = functionsModal.querySelector(".options-search-input");
+    const mainOptionSet = functionsModal.querySelector(".function-options.options");
+    const subOptionSets = functionsModal.querySelectorAll(".sub-options");
+    const allOptionSets = [...subOptionSets, mainOptionSet];
+    const allOptions = functionsModal.querySelectorAll(".option");
 
-    optionLabel.addEventListener("click", () => {
-        subOptionSet.style.display = "flex";
-        const subOptionBackArrow = subOptionSet.querySelector(".sub-options-head > .arrow");
 
-        subOptionBackArrow.addEventListener("click", () => {
-            subOptionSet.style.display = "none";
+    allOptionSets.forEach(optionSet => {
+        optionSet.close = () => {
+            optionSet.classList.remove("show-flex");
+    
+            // Reset all form inputs to their default values
+            const formInputs = document.querySelectorAll(".form-input");
+            formInputs.forEach(formInput => {
+                formInput.value = formInput.dataset.default ?? formInput.value;
+            });
+        };
+    
+        optionSet.open = () => {
+            optionSet.classList.add("show-flex");
+        };
+    });
+    
+    
+    subOptionSets.forEach(subOptionSet => {
+        const formFieldsDoneBtns = subOptionSet.querySelectorAll(".form-fields .done-btn");
+    
+        formFieldsDoneBtns.forEach(doneBtn => {
+            doneBtn.addEventListener("click", () => {
+                subOptionSet.close();
+            });
         });
     });
+    
+    
+    
+    functionsModal.reset = () => {
+        subOptionSets.forEach(subOptionSet => {
+            subOptionSet.close();
+        });
+    }
+    
+    functionsModal.open = () => {
+        functionsModal.classList.add("show-flex");
+    }
+    
+    functionsModal.close = () => {
+        functionsModal.classList.remove("show-flex");
+    }
+    
+    
+    // Function to handle search functionality
+    optionsSearchInput.addEventListener('input', function () {
+        const filter = this.value.toLowerCase();
+        const targetOptionSet = getVisibleElementWithHighestZIndex(allOptionSets);
+    
+        targetOptionSet.querySelectorAll('.option').forEach(item => {
+            const text = item.textContent.toLowerCase();
+            item.style.display = text.includes(filter) ? '' : 'none';
+        });
+    });
+    
+    
+    allOptions.forEach(option => {
+        const optionLabel = option.querySelector(".option-label");
+        const subOptionSet = option.querySelector(".sub-options");
+        if (!subOptionSet) return;
+    
+        optionLabel.addEventListener("click", () => {
+            // Revert the scroll of the parent container
+            option.parentElement.scrollTop = 0;
+    
+            subOptionSet.open();
+            const subOptionBackArrow = subOptionSet.querySelector(".sub-options-head > .arrow");
+    
+            subOptionBackArrow.addEventListener("click", () => {
+                subOptionSet.close();
+            });
+        });
+    });
+    
+    
+    functionsModalCloseBtn.addEventListener("click", () => {
+        functionsModal.close();
+    });
+    
 });
-
--- apps/risk_management/static/risk_management/scripts/riskProfileCreate.js
+++ apps/risk_management/static/risk_management/scripts/riskProfileCreate.js
@@ -2,17 +2,96 @@ const profileCreateModal = document.querySelector('#profileCreateModal');
 const profileCreateForm = profileCreateModal.querySelector('#profile-create-form');
 const criteriaCreationSectionToggle = profileCreateForm.querySelector('#criteria-creation-section-toggle');
 const criteriaCreationSection = profileCreateForm.querySelector("#criteria-creation-section");
-const criteriaCreationFields = criteriaCreationSection.querySelector("#criteria-creation-fields");
-const functionInputs = criteriaCreationFields.querySelectorAll("input.function-input");
+const criteriaCreationFieldContainer = criteriaCreationSection.querySelector("#criteria-creation-fields");
+const criteriaCreationFormFields = criteriaCreationFieldContainer.querySelector(".form-fields");
+const functionInputs = criteriaCreationFormFields.querySelectorAll("input.function-input");
+const criteriaAddButton = criteriaCreationFieldContainer.querySelector(".add-btn");
 
 
-criteriaCreationSectionToggle.addEventListener("click", () => {    
+function resetFormFields(formFieldsContainer) {
+    const formInputs = formFieldsContainer.querySelectorAll(".form-input");
+
+    formInputs.forEach(formInput => {
+        formInput.value = null;
+    });
+}
+
+
+function getCriteriaCreationFormInputsData(formInputs) {
+    const data = {}
+
+    formInputs.forEach(input => {
+        const options = input.dataset.options ?? null;
+        if (options) {
+            data[input.name] = {
+                "name": input.value,
+                "options": JSON.parse(options),
+            }
+        }
+        else {
+            data[input.name] = input.value
+        }
+    });
+
+    return data;
+}
+
+
+function captureFunctionSelection(functionsModal, inputField) {
+    const options = functionsModal.querySelectorAll(".option");
+
+    options.forEach(option => {
+        const optionSubOptions = option.querySelector(".sub-options");
+
+        if (!optionSubOptions) {
+            option.addEventListener("click", () => {
+                const functionName = option.dataset.function ?? null;
+                if (!functionName) return;
+
+                inputField.value = functionName;
+                inputField.dataset.options = null;
+
+                functionsModal.close();
+            });
+            return;
+        };
+
+        const doneBtns = functionsModal.querySelectorAll(".done-btn");
+        doneBtns.forEach(doneBtn => {
+            doneBtn.addEventListener("click", () => {
+                const parentFormFieldContainer = doneBtn.closest(".form-fields");
+                const formFieldsData = getSubOptionFormFieldsData(parentFormFieldContainer);
+
+                inputField.value = formFieldsData.name;
+                inputField.dataset.options = JSON.stringify(formFieldsData.options);
+
+                functionsModal.close();
+            });
+        });
+    });
+};
+
+
+criteriaCreationSectionToggle.addEventListener("click", () => {
     criteriaCreationSection.classList.toggle("show-block");
 });
 
 
 functionInputs.forEach((input) => {
     input.addEventListener("click", () => {
-        functionsModal.classList.toggle("show-flex");
+        const functionsModal = input.parentElement.querySelector(".functions-modal");
+        if (!functionsModal) return;
+
+        functionsModal.open();
+        captureFunctionSelection(functionsModal, input);
     });
+});
+
+
+criteriaAddButton.addEventListener("click", () => {
+    const formInputs = criteriaCreationSection.querySelectorAll("#criteria-creation-fields > .form-fields > .form-field > .form-input");
+    const criteriaData = getCriteriaCreationFormInputsData(formInputs);
+    
+    criteriaCreationSection.classList.remove("show-block");
+    resetFormFields(criteriaCreationFormFields);
 });

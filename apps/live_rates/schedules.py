a4cd8cfb3dbecbffec5e194f45b1cb06c9e2f845
tioluwa <tioluwa.dev@gmail.com>
2024-09-23 00:07:22 +0100

    Extended background update rates timeout to allow for long running rates updates in the case the rates data fetched is large

--- apps/live_rates/schedules.py
+++ apps/live_rates/schedules.py
@@ -30,8 +30,8 @@ def schedule_stock_rates_update(
         start_date=start_date,
         end_date=end_date,
         q_options={
-            "timeout": 120,
-            "retry": 150,
+            "timeout": 300,
+            "retry": 350,
             "save": True,
         },
         schedule_type="I",

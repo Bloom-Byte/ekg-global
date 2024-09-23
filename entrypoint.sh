54da5108b4ef3eddcd8f5a48ff8cd69cf9388ccc
agentops <abdallahmohammed79@yahoo.com>
2024-09-23 14:10:54 +0100

    latest update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,4 @@ python manage.py migrate
 python manage.py collectstatic --noinput 
 python manage.py update_rates --schedule --latest --cron "0 * * * *"
 python manage.py runserver 0.0.0.0:8000
+python manage.py qcluster &

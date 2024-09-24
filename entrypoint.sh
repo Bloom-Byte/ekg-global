9afaba871770d69982aca5131b3ae407b19b35c4
tioluwa <tioluwa.dev@gmail.com>
2024-09-24 18:07:40 +0100

    What: Updated MGLink provider clientaccount for providerider's timezone when parsing datetime values in api response. Switch background task back to running every 5 mins

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 python manage.py migrate 
 python manage.py collectstatic --noinput 
 python manage.py update_rates # Fetches and updates to last 30days stock rates. Populates db with stocks if they do not exist
-python manage.py update_rates --schedule --latest --cron "*/5 * * * *" # Schedule background task to update stocks rate to latest rate every 1hr
+python manage.py update_rates --schedule --latest --cron "*/5 * * * *" # Schedule background task to update stocks rate to latest rate every 5mins
 python manage.py index_stocks # Update stocks' indices
 python manage.py runserver 0.0.0.0:8000
 python manage.py qcluster &
